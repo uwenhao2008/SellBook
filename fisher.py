e40be725eb70edb2a11d6c1c2f558c38a2cbe8c8
Fidea <uwenhao2008@qq.com>
2018-09-14 10:42:13 +0800

    init webserver

---
+++ fisher.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'MacVSPC'
+__mtime__ = '2018-09-14'
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛
+"""
+from flask import Flask
+# 实例化
+app = Flask(__name__)
+
+app.run()
